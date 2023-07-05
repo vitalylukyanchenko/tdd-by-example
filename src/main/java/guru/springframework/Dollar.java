a8c13bd26a86a59b144efeca027b38eacef709e3
VitalyLukyanchenko <vitaly.lukyanchenko@shieldfc.com>
2023-07-05 18:13:23 +0300

    video 18

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar)  object;
+        return amount == dollar.amount;
+    }
+}
---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,24 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class MoneyTest {
+
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product =  five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+}
